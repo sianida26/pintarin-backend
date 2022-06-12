92afea9ea3f6eb7920b20d194f4b11e2aeaab94c
Chesa <chesakeselekkebo@gmail.com>
2022-06-12 07:02:03 +0700

    Setup documentation engine

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,31 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="Pintarin Documentation",
+ *      description="Pintarin OpenApi description",
+ *      @OA\Contact(
+ *          email="chesanurhidayat@gmail.com"
+ *      ),
+ *      @OA\License(
+ *          name="Apache 2.0",
+ *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *      )
+ * )
+ *
+ * @OA\Server(
+ *      url=L5_SWAGGER_CONST_HOST,
+ *      description="Pintarin API Server"
+ * )
+
+ *
+ * @OA\Tag(
+ *     name="Development Endpoints",
+ *     description="API Endpoints for development purposes only"
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
--- app/Http/Controllers/DevController.php
+++ app/Http/Controllers/DevController.php
@@ -6,7 +6,18 @@ use Illuminate\Http\Request;
 
 class DevController extends Controller
 {
-    //
+    /**
+     * @OA\Get(
+     *      path="/__test__",
+     *      tags={"Development Endpoints"},
+     *      summary="Test API Endpoint",
+     *      description="Returns OK",
+     *      @OA\Response(
+     *          response=200,
+     *          description="OK",
+     *      ),
+     *     )
+     */
     public function test(){
         return 'OK';
     }
