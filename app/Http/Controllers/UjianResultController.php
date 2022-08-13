daec43054f11650586edc33272dd949d1d02fecb
sianida26 <chesakeselekkebo@gmail.com>
2022-08-14 05:50:25 +0700

    coba

--- app/Http/Controllers/UjianResultController.php
+++ app/Http/Controllers/UjianResultController.php
@@ -191,6 +191,9 @@ class UjianResultController extends Controller
         if (!$ujian || !$ujian->isUjian || $ujian->guru->id !== $guru->id)
             return response()->json(['message' => 'Ujian tidak ditemukan'], 404);
 
+        Debugbar::info($ujianResult->getAnswerBySoalId($id));
+        Debugbar::info($ujianResult->answers);
+
         $soals = $ujian->soals
             ->map(fn($soal) => [
                 'type' => $soal->type,
