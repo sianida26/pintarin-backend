3bb54b1cfd2e87d8a76a9abdc52068367a81f770
sianida26 <chesakeselekkebo@gmail.com>
2022-08-17 13:35:54 +0700

    Add details

--- app/Http/Controllers/UjianResultController.php
+++ app/Http/Controllers/UjianResultController.php
@@ -308,15 +308,15 @@ class UjianResultController extends Controller
 
                 $deskripsi = "";
                 if ($ujian->category === "numerasi"){
-                    if ($resultCategory === "A") $deskripsi = "Peserta didik mampu bernalar untuk menyelesaikan masalah kompleks serta nonrutin berdasarkan konsep matematika yang dimilikinya.";
-                    else if ($resultCategory === "B") $deskripsi = "Peserta didik mampu mengaplikasikan pengetahuan matematika yang dimiliki dalam konteks yang lebih beragam.";
-                    else if ($resultCategory === "C") $deskripsi = "Peserta didik memiliki kemampuan dasar matematika: komputasi dasar dalam bentuk persamaan langsung, konsep dasar terkait geometri dan statistika, serta menyelesaikan masalah matematika sederhana yang rutin.";
-                    else if ($resultCategory === "D") $deskripsi = "Peserta didik hanya memiliki pengetahuan matematika yang terbatas (penguasaan konsep yang parsial dan keterampilan komputasi yang terbatas).";
+                    if ($resultCategory === "Mahir") $deskripsi = "Peserta didik mampu bernalar untuk menyelesaikan masalah kompleks serta nonrutin berdasarkan konsep matematika yang dimilikinya.";
+                    else if ($resultCategory === "Cakap") $deskripsi = "Peserta didik mampu mengaplikasikan pengetahuan matematika yang dimiliki dalam konteks yang lebih beragam.";
+                    else if ($resultCategory === "Dasar") $deskripsi = "Peserta didik memiliki kemampuan dasar matematika: komputasi dasar dalam bentuk persamaan langsung, konsep dasar terkait geometri dan statistika, serta menyelesaikan masalah matematika sederhana yang rutin.";
+                    else if ($resultCategory === "Perlu Intervensi Khusus") $deskripsi = "Peserta didik hanya memiliki pengetahuan matematika yang terbatas (penguasaan konsep yang parsial dan keterampilan komputasi yang terbatas).";
                 } else if ($ujian->category === "literasi"){
-                    if ($resultCategory === "A") $deskripsi = "Peserta didik mampu mengitegrasikan beberapa informasi lintas teks; mengevaluasi isi, kualitas, cara penulisan suatu teks, dan bersikap reflektif terhadap isi teks.";
-                    else if ($resultCategory === "B") $deskripsi = "Peserta didik mampu membuat interpretasi dari informasi implisit yang ada dalam teks; mampu membuat simpulan dari hasil integrasi beberapa informasi dalam suatu teks.";
-                    else if ($resultCategory === "C") $deskripsi = "Peserta didik mampu menemukan dan mengambil informasi eksplisit yang ada dalam teks serta membuat interpretasi sederhana.";
-                    else if ($resultCategory === "D") $deskripsi = "Peserta didik belum mampu menemukan dan mengambil informasi eksplisit yang ada dalam teks ataupun membuat interpretasi sederhana.";
+                    if ($resultCategory === "Mahir") $deskripsi = "Peserta didik mampu mengitegrasikan beberapa informasi lintas teks; mengevaluasi isi, kualitas, cara penulisan suatu teks, dan bersikap reflektif terhadap isi teks.";
+                    else if ($resultCategory === "Cakap") $deskripsi = "Peserta didik mampu membuat interpretasi dari informasi implisit yang ada dalam teks; mampu membuat simpulan dari hasil integrasi beberapa informasi dalam suatu teks.";
+                    else if ($resultCategory === "Dasar") $deskripsi = "Peserta didik mampu menemukan dan mengambil informasi eksplisit yang ada dalam teks serta membuat interpretasi sederhana.";
+                    else if ($resultCategory === "Perlu Intervensi Khusus") $deskripsi = "Peserta didik belum mampu menemukan dan mengambil informasi eksplisit yang ada dalam teks ataupun membuat interpretasi sederhana.";
                 }
 
                 return [
